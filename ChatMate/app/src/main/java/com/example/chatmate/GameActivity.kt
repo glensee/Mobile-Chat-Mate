de09f5a95851d750d67d6899e8ee57ae17209583
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2021-03-10 02:21:50 +0800

    add on click listener to tiles

--- ChatMate/app/src/main/java/com/example/chatmate/GameActivity.kt
+++ ChatMate/app/src/main/java/com/example/chatmate/GameActivity.kt
@@ -9,15 +9,18 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.chatmate.databinding.ActivityGameBinding
 import com.github.bhlangonijr.chesslib.Board
+import com.github.bhlangonijr.chesslib.Piece
 import com.github.bhlangonijr.chesslib.Square
 import com.github.bhlangonijr.chesslib.move.Move
 
 
 class GameActivity : AppCompatActivity() {
-    lateinit var gameBinding: ActivityGameBinding
-    lateinit var board: Board
+    private val checkPattern = arrayListOf<Int>(0,2,4,6,9,11,13,15,16,18,20,22,25,27,29,31,32,34,36,38,41,43,45,47,48,50,52,54,57,59,61,63)
+    private lateinit var gameBinding: ActivityGameBinding
+    private lateinit var board: Board
     // Keeps track of all generated Image Button Tiles
-    val chessTiles = ArrayList<ImageButton>()
+    private val chessTiles = ArrayList<ImageButton>()
+    private var tileSelectedIndex = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +33,6 @@ class GameActivity : AppCompatActivity() {
         generateChessBoardTileButtons()
         // Set the Image in the Image button based on pieces in board class
         renderBoardState()
-
-
-        board.doMove(Move(Square.E2, Square.E4))
-        renderBoardState()
     }
 
     private fun generateChessBoardTileButtons () {
@@ -42,7 +41,7 @@ class GameActivity : AppCompatActivity() {
         // Temporary Track the Sequence of Image Button Generation
         val boardArrayList = ArrayList<ArrayList<ImageButton>>()
         // LayoutParams used to "Style" the Generated Image Buttons & Linear Layouts
-        var params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f)
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f)
 
         // Generate Image Button by Rows
         for (rowIndex in 0 until 8) {
@@ -63,22 +62,6 @@ class GameActivity : AppCompatActivity() {
                 chessTile.scaleType = ImageView.ScaleType.FIT_XY
                 // Set Padding Within the Image Button
                 chessTile.setPadding(25,25,25,25)
-
-                // Set Background Color of the Image Button to Create the Chess Board Look
-                if (rowIndex % 2 == 0) {
-                    if (colIndex % 2 == 0) {
-                        chessTile.setBackgroundColor(Color.parseColor("#EEEED2"))
-                    } else {
-                        chessTile.setBackgroundColor(Color.parseColor("#769656"))
-                    }
-                } else {
-                    if (colIndex % 2 == 0) {
-                        chessTile.setBackgroundColor(Color.parseColor("#769656"))
-                    } else {
-                        chessTile.setBackgroundColor(Color.parseColor("#EEEED2"))
-                    }
-                }
-
                 // Insert Tile into ArrayList to keep track of its Generating Sequence
                 rowArrayList.add(chessTile)
                 // Insert Tile into the Linear Layout
@@ -92,9 +75,14 @@ class GameActivity : AppCompatActivity() {
 
         // Using the Temporary Sequence, Add Tiles into the Main ArrayList That Tracks each Image Button
         // The Final Sequence matches the Sequence Used in the Board Class
-        for(i in boardArrayList.size - 1 downTo 0){
+        for (i in boardArrayList.size - 1 downTo 0) {
             for (tile in boardArrayList[i]) {
+                tile.tag = chessTiles.size
                 chessTiles.add(tile)
+                tile.setOnClickListener {
+                    val tileIndex = it.tag.toString().toInt()
+                    selectTileAtIndex(tileIndex)
+                }
             }
         }
     }
@@ -124,8 +112,39 @@ class GameActivity : AppCompatActivity() {
                 "BLACK_KING" -> chessTile.setImageResource(R.drawable.b_king_2x_ns)
                 else -> chessTile.setImageResource(0)
             }
+
+            // Set the Background for Image Button
+            if (i in checkPattern) {
+                chessTile.setBackgroundColor(Color.parseColor("#769656"))
+            } else {
+                chessTile.setBackgroundColor(Color.parseColor("#EEEED2"))
+            }
         }
 
         gameBinding.textView.text = board.toString()
+    }
+
+    private fun selectTileAtIndex (tileIndex: Int) {
+        val sideToMove = board.sideToMove
+        val pieceAtIndex = board.getPiece(Square.squareAt(tileIndex))
+
+        // Check if a Chess Piece was Previously Selected
+        if (tileSelectedIndex == -1) {
+            // Chess Piece not Previously Selected
+            // Check if Selected Tile has a Chess Piece Belonging to Current Side to Move
+            if (pieceAtIndex != Piece.NONE && pieceAtIndex.pieceSide == sideToMove) {
+                // Highlight Tile that was Selected
+                chessTiles[tileIndex].setBackgroundColor(Color.parseColor("#F6F669"))
+                // Save the Index of the Selected Tile
+                tileSelectedIndex = tileIndex
+            }
+
+        } else {
+            // Chess Piece Previously Selected
+            if(board.doMove(Move(Square.squareAt(tileSelectedIndex), Square.squareAt(tileIndex)))) {
+                renderBoardState()
+                tileSelectedIndex = -1
+            }
+        }
     }
 }
