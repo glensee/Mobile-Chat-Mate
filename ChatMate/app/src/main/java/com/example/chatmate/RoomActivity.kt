3e2b56de670d503d21fa0bd59a2901f6055cc89c
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2021-03-16 19:52:54 +0800

    added google config json

--- ChatMate/app/src/main/java/com/example/chatmate/RoomActivity.kt
+++ ChatMate/app/src/main/java/com/example/chatmate/RoomActivity.kt
@@ -1,24 +1,24 @@
-package com.example.chatmate
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-
-class RoomActivity : AppCompatActivity() {
-
-    lateinit var playerName: String
-    lateinit var roomName: String
-    lateinit var db: FirebaseDatabase
-    lateinit var  roomRef: DatabaseReference
-    lateinit var roomsRef: DatabaseReference
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_room)
-
-
-        db = FirebaseDatabase.getInstance()
-//        playerName =
-    }
-}
+//package com.example.chatmate
+//
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+//import com.google.firebase.database.DatabaseReference
+//import com.google.firebase.database.FirebaseDatabase
+//
+//class RoomActivity : AppCompatActivity() {
+//
+//    lateinit var playerName: String
+//    lateinit var roomName: String
+//    lateinit var db: FirebaseDatabase
+//    lateinit var  roomRef: DatabaseReference
+//    lateinit var roomsRef: DatabaseReference
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_room)
+//
+//
+//        db = FirebaseDatabase.getInstance()
+////        playerName =
+//    }
+//}
