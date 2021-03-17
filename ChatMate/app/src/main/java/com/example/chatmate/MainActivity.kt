4537a8c1e345f2a0be751be5b2c73e433b2335ad
Cliffenlee <cliffen.lee.2018@sis.smu.edu.sg>
2021-03-17 15:28:49 +0800

    implemented room list

--- ChatMate/app/src/main/java/com/example/chatmate/MainActivity.kt
+++ ChatMate/app/src/main/java/com/example/chatmate/MainActivity.kt
@@ -1,24 +1,49 @@
 package com.example.chatmate
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatmate.databinding.ActivityMainBinding
 import android.content.Intent
+import android.content.SharedPreferences
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     // declaring binding for data binding
     private lateinit var binding:ActivityMainBinding
+    private lateinit var db: FirebaseFirestore
+    private var currentUUID = ""
+    private var currentUser = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // set data binding
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = Firebase.firestore
+//        FirebaseFirestore.setLoggingEnabled(true)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // remove player from online players list in firebase
+        Log.i("cliffen",currentUUID)
+        db.collection("players").document(currentUUID)
+            .delete()
+            .addOnSuccessListener { Log.d("cliffen", "DocumentSnapshot successfully deleted!") }
+            .addOnFailureListener { e -> Log.w("cliffen", "Error deleting document", e) }
+
+        Log.i("cliffen", "past that stage")
     }
 
     fun NavigateToGame(view: View) {
@@ -27,5 +52,94 @@ class MainActivity : AppCompatActivity() {
         startActivity(it)
     }
 
+    fun NavigateToHome(view: View) {
+        // get username and password
+        val name = binding.name.text.toString().trim()
 
+        // if username empty
+        if (name.length <= 0) {
+            Toast.makeText(this, "Please enter username and password!", Toast.LENGTH_SHORT)
+                .show()
+        } else {
+            val sharedPref = this.getSharedPreferences("usernames", Context.MODE_PRIVATE)
+            val existingUUID = sharedPref.getString(name,  null)
+            val uuid = UUID.randomUUID().toString()
+            currentUser = name
+//            Log.i("cliffen", existingUUID)
+
+            // user doesn't exist in shared pref
+            if (existingUUID == null) {
+                Log.i("cliffen", "doesnt exist")
+                with (sharedPref.edit()) {
+                    putString(currentUser, uuid)
+                    apply()
+                }
+                currentUUID = uuid
+            } else {
+                Log.i("cliffen", "exist")
+                // if user has been created in shared pref before
+                currentUUID = existingUUID
+            }
+
+            Log.i("cliffen", currentUUID)
+         // Create online player in firebase
+            val player = hashMapOf(
+                "name" to currentUser,
+                "id" to currentUUID
+            )
+
+            // Add a new document with a generated ID
+            db.collection("players")
+                .document(currentUUID)
+                .set(player)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("cliffen", "Document added!")
+                }
+                .addOnFailureListener { e ->
+                    Log.d("cliffen", "Error adding document", e)
+                }
+
+            val it = Intent(this, RoomActivity::class.java)
+            it.putExtra("name", currentUser)
+            it.putExtra("uuid", currentUUID)
+            startActivity(it)
+        }
+    }
+
+//    fun deleteUser(view: View) {
+//        currentUser = binding.name.text.toString().trim()
+//        val sharedPref = this.getSharedPreferences("usernames", Context.MODE_PRIVATE)
+//
+//        val existingUUID = sharedPref.getString(currentUser, null)
+//        val uuid = UUID.randomUUID().toString()
+//
+//        // user doesn't exist in shared pref
+//        if (existingUUID == null) {
+//            Log.i("cliffen", "doesnt' exist")
+////            with (sharedPref.edit()) {
+////                putString(currentUser, uuid)
+////                apply()
+////            }
+//            currentUUID = uuid
+//        } else {
+//            Log.i("cliffen", "exist")
+//            // if user has been created in shared pref before
+//            currentUUID = existingUUID
+//
+//            // remove player from online players list in firebase
+//            Log.i("cliffen",currentUUID)
+//            db.collection("players").document(currentUUID)
+//                .delete()
+//                .addOnSuccessListener { Log.d("cliffen", "DocumentSnapshot successfully deleted!") }
+//                .addOnFailureListener { e -> Log.w("cliffen", "Error deleting document", e) }
+//
+//            Log.i("cliffen", "past that stage")
+//        }
+//
+//        Log.i("cliffen", currentUUID)
+////        with(sharedPref.edit()) {
+////            clear()
+////            apply()
+////        }
+//    }
 }
--- ChatMate/app/src/main/java/com/example/chatmate/RoomActivity.kt
+++ ChatMate/app/src/main/java/com/example/chatmate/RoomActivity.kt
@@ -1,15 +1,91 @@
 package com.example.chatmate
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chatmate.databinding.ActivityRoomBinding
+import com.google.firebase.firestore.*
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class RoomActivity : AppCompatActivity() {
 
-    lateinit var playerName: String
-    lateinit var roomName: String
+    private lateinit var binding: ActivityRoomBinding
+    private lateinit var db: FirebaseFirestore
+    private lateinit var roomRef: CollectionReference
+    private lateinit var roomsAdapter:ArrayAdapter<String>
+    private var roomList = ArrayList<String>()
+    private var playerName = ""
+    private var uuid = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_room)
+        binding = ActivityRoomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // receive name and uuid from intent
+        playerName = intent.getStringExtra("name").toString()
+        uuid = intent.getStringExtra("uuid").toString()
+
+        Log.i("cliffen", "player name:" + playerName)
+
+        // initialize firestore
+        db = Firebase.firestore
+        FirebaseFirestore.setLoggingEnabled(true)
+
+        // listen to room changes
+        roomRef = db.collection("rooms")
+        roomRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w("cliffen", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null) {
+                var tempRoomList = ArrayList<String>()
+                var snapshotDocuments = snapshot.documents
+                if (snapshotDocuments !== null) {
+                    for (documentSnapshot in snapshotDocuments) {
+                        val docSnap = documentSnapshot.data
+                        if (docSnap != null) {
+                            val player1 = docSnap["player1"].toString()
+                            val player2 =  docSnap["player2"].toString()
+                            val roomDesc = player1 + "'s room"
+                            tempRoomList.add(roomDesc)
+                        }
+                    }
+                }
+
+                roomList = tempRoomList
+
+                // update list view
+                roomsAdapter = ArrayAdapter<String>(this, R.layout.simple_list_item_1, roomList)
+                binding.rooms.adapter = roomsAdapter
+                roomsAdapter.notifyDataSetChanged()
+
+                Log.i("cliffen", roomList.toString())
+            } else {
+                Log.d("cliffen", "Current data: null")
+            }
+        }
+
     }
+    
+    fun createRoom(view: View) {
+        val room = hashMapOf(
+            "roomId" to uuid,
+            "player1" to playerName
+        )
+
+        db.collection("rooms").document(uuid)
+            .set(room)
+            .addOnSuccessListener { Log.i("cliffen", "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.i("cliffen", "Error writing document", e) }
+    }
+    
 }
